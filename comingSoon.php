38d46369d13724929a55a2aec7f8c7775588c0bd
Anmol Kumar Shrivastav <anmolshrivastav.08@gmail.com>
2022-06-09 19:27:27 +0530

    improved comingSoon page and replaced footer social media link with comingSoon

--- comingSoon.php
+++ comingSoon.php
@@ -1,1 +1,49 @@
-<h1>Coming Soon.</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming Soon</title>
+    <link rel="icon" href="images/logo1.png"> 
+    <link rel="stylesheet" type="text/css" href="files/css/bootstrap.css">
+	<script type="text/javascript" src="files/js/jquery.js"></script>
+	<script type="text/javascript" src="files/js/bootstrap.bundle.js"></script>
+
+    <style type="text/css">
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .bg-yellow  {
+            background-color: #FFED46;
+        }
+        .container-fluid{
+            width: 100vw;
+            height: 100vh;
+            margin: 0;
+            box-sizing: border-box;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container{
+            width: fit-content;
+            height: fit-content;
+            margin: 0;
+            color: white;
+            box-sizing: border-box;
+            padding: 10px 15px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid bg-yellow">
+        <div class="container bg-dark">
+            <p>We are working on this feature it will be available for you very soon.</p>
+            <p>You Stay Tuned</p>
+            <img src="thankyou.gif" class="img-fluid"/>
+        </div>
+    </div>
+</body>
+</html>
